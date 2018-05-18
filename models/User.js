6ab9d64712f19f0fd6279602c4f3843c97151973
fangzhzh <fangzhzh@gmail.com>
2018-05-19 07:17:26 +1200

    - add back User

---
+++ models/User.js
@@ -0,0 +1,41 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('User', {
+    UserID: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    type: {
+      type: DataTypes.STRING(64),
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    deviceID: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    deviceType: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    pushType: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    pushToken: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    }
+  }, {
+    tableName: 'User'
+  });
+};
