510d66f0860ff5f290926dba1c64af52a58ccc03
fangzhzh <fangzhzh@gmail.com>
2018-05-20 22:44:15 +1200

    - todo controller

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,6 +1,23 @@
 const Todo = require("../models").Todo;
+const getByType = async function(req, res, type) {
+  res.setHeader("Content-type", "application/json");
+  [err, todos] = await to(
+    Todo.findAll({ UserID: req.user.UserID, todoType: type })
+  );
+  if (err) return ReE(res, err, 404);
+  const todos_json = todos.map(todo => todo.toWeb());
+  return ReS(res, { todos: todos_json });
+};
+
+const getChi = async function(req, res) {
+  return getByType("chi");
+};
+module.exports.getChi = getChi;
+
+const getZhang = async function(req, res) {
+  return getByType("zhang");
+};
+module.exports.getZhang = getZhang;
+
 const get = async function(req, res) {};
 module.exports.get = get;
-
-const getAll = async function(req, res) {};
-module.exports.getAll = getAll;
--- models/Todo.js
+++ models/Todo.js
@@ -1,50 +1,60 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Todo', {
-    TodoID: {
-      type: DataTypes.BIGINT,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+  let Model = sequelize.define(
+    "Todo",
+    {
+      TodoID: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      Content: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      CreatedTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      UpdatedTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      Status: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true
+      },
+      Type: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true
+      },
+      TodoType: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+        defaultValue: "chi",
+        primaryKey: true
+      },
+      Note: {
+        type: DataTypes.STRING(1024),
+        allowNull: true
+      },
+      Todocol: {
+        type: DataTypes.STRING(45),
+        allowNull: true
+      }
     },
-    Content: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    CreatedTime: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    UpdatedTime: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    Status: {
-      type: DataTypes.INTEGER(11),
-      allowNull: true
-    },
-    Type: {
-      type: DataTypes.INTEGER(11),
-      allowNull: true
-    },
-    TodoType: {
-      type: DataTypes.STRING(45),
-      allowNull: false,
-      defaultValue: 'chi',
-      primaryKey: true
-    },
-    Note: {
-      type: DataTypes.STRING(1024),
-      allowNull: true
-    },
-    Todocol: {
-      type: DataTypes.STRING(45),
-      allowNull: true
+    {
+      tableName: "Todo"
     }
-  }, {
-    tableName: 'Todo'
-  });
+  );
+
+  Model.prototype.toWeb = function(pw) {
+    let json = this.toJSON();
+    return json;
+  };
+  return Model;
 };
